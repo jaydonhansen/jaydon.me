b39bd25ed39b2901c3f3150834b8800adb47fd36
Jaydon Hansen <49255230+jaydonhansen@users.noreply.github.com>
2022-02-05 23:02:48 +1000

    Here I stand and look at my life

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import numel from "./numel.gif";
 import "./App.css";
+import Spotify from "react-spotify-embed";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={numel} className="Numel" alt="hello there" />
+        <Spotify
+          wide
+          link="https://open.spotify.com/track/2qUWAMtLCCfcnIaqANz1lR?si=d4e6fb362c2e4fa1"
+          width="50%"
+        />
       </header>
     </div>
   );
