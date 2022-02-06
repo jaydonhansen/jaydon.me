989fbac19b1246afb5e07bdaa406c57da46dbbe5
Jaydon Hansen <49255230+jaydonhansen@users.noreply.github.com>
2022-02-06 21:44:34 +1000

    help i'm dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import ReactPlayer from "react-player/youtube";
-
+import { MoonIcon } from "@heroicons/react/outline";
+import { useTheme } from "./hooks/useDarkMode";
 
 function App() {
+  const { changeTheme } = useTheme();
   const numel = "/img/numel.gif";
   return (
-    <div className="text-center	">
-      <div className="grid place-items-center h-screen bg-main-background">
-        <div className="grid place-items-center max-w-xl">
-          <img src={numel} className= "p-32" alt="hello there" />
-          <ReactPlayer url="https://www.youtube.com/watch?v=T0ee9RaUJqE" volume={0.2} controls={true}/>
+    <div className="App">
+      <div className="flex h-screen bg-slate-200 dark:bg-slate-900">
+        <div className="fixed top-2 right-2">
+          <button
+            onClick={() => changeTheme()}
+            type="button"
+            className=" rounded-full p-2"
+          >
+            <MoonIcon className="h-5 w-5 text-slate-900 dark:text-slate-200" />
+          </button>
+        </div>
+
+        <div className="grid m-auto screen">
+          <div className="grid place-concent-center max-w-xl">
+            <img src={numel} className="p-32" alt="hello there" />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/hooks/useDarkMode.ts
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export type ThemeType = "light" | "dark";
+
+export function useTheme(): {
+  theme: ThemeType;
+  changeTheme: () => void;
+  } {
+  const [theme, setTheme] = useState<ThemeType>("light");
+
+  function changeTheme() {
+    if (theme === "light") {
+      document.querySelector("html")?.classList?.add?.("dark");
+      setTheme("dark");
+    } else {
+      document.querySelector("html")?.classList?.remove?.("dark");
+      setTheme("light");
+    }
+  }
+
+  useEffect(() => {
+    const darkOS = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (darkOS) {
+      changeTheme();
+    }
+  }, []);
+
+  return { theme, changeTheme };
+}
